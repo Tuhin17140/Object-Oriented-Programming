c9d940d8d23a7a77b6e3fbfe353dd2e38c972bec
tuhin <leoboy1007@gmail.com>
2020-09-28 13:57:44 +0600

    someChanges

--- rgbColor.php
+++ rgbColor.php
@@ -1,5 +1,5 @@
 <?php
-
+/*
 class RGB
 {
     private $color;
@@ -56,6 +56,53 @@ $myColor->setColor('#ffef27');
 $myColor->readRGBColor();
 $myColor->getBlue();
 
+$myColor->readRGBColor();*/
+
+
+
+class RGB{
+    private $color;
+    private $red;
+    private $green;
+    private $blue;
+
+    function __construct($colorCode)
+    {
+        $this->color = ltrim($colorCode,'#');
+        $this->parseColor();
+    }
+
+    function getColor(){
+        echo $this->color."\n";
+    }
+    function getRGBColor(){
+        print_r(array($this->red,$this->green,$this->blue))."\n";
+    }
+    function readRGBColor(){
+        echo "Red = $this->red\nGreen = $this->green\nBlue = $this->blue\n";
+    }
+    function setColor($colorCode){
+        $this->color = ltrim($colorCode,'#');
+        $this->parseColor();
+    }
+    private function parseColor(){
+        if($this->color){
+            list($this->red,$this->green,$this->blue) = sscanf($this->color,"%02x%02x%02x");
+        }
+    }
+    function getRed(){
+        echo $this->red."\n";
+    }
+    function getGreen(){
+        echo $this->green."\n";
+    }
+    function getBlue(){
+        echo $this->blue."\n";
+    }
+}
+
+$myColor = new RGB("#ffef27");
+$myColor->getColor();
+$myColor->getRGBColor();
 $myColor->readRGBColor();
-
-
+$myColor->getRed();
